d34228d68e848d9a8e0ab7c3d133e23f74a1f34b
Arun-Pratap-Khichi <153052885+Arun-Pratap-Khichi@users.noreply.github.com>
2025-06-25 17:24:33 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 dotenv.config();
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.Frontend_Url],
   },
 });
 
